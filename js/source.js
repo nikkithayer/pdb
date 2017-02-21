fef5982f1ec220b8c7dca8f1d98a6cb08ca59909
Nicole Thayer <nicole@carbonfive.com>
2017-02-20 19:52:26 -0800

    Added rotation and label text, successful mimic of text replace

--- js/source.js
+++ js/source.js
@@ -1,25 +1,52 @@
 $( document ).ready(function() {								
 
-$("button.next").addClass("inactive");
-var buttonRating = 0;
+$.getJSON( "../questions.json", function( json ) {
+	var questions = json;
+	var quizPosition = 0;
+	iterateQuizPosition();
 
-console.log($("div.response").width());
+	var buttonRating = 0;
 
-$("fieldset button").click(
-	function(){
-		$(this).addClass("active");
-		$(this).prevAll().addClass("active");
-		$(this).nextAll().removeClass("active");
-		$("button.next").removeClass("inactive");
-		$("#display-progress").addClass("click");
-		buttonRating = $(this).text();
-		console.log($("#display-progress").children("g"));
+	$("fieldset button").click(
+		function(){
+			$(this).addClass("active");
+			$(this).prevAll().addClass("active");
+			$(this).nextAll().removeClass("active");
+			$("button.next").removeClass("inactive");
+			$("#display-progress").addClass("click");
+			buttonRating = $(this).text();
+			console.log($("#display-progress").children("g"));
+		}
+	);	
+	$("button.next").click(
+		function(){
+			iterateQuizPosition();
+			resetQuizStatus();
+		}
+	);
+
+	function iterateQuizPosition(){
+		if(quizPosition<questions.length){
+			var quizStep = quizPosition+1;
+			$("h1#question-title").text(quizStep + ". " + questions[quizPosition].title);
+			$("p#question-description").text(questions[quizPosition].description);
+			rotateDartboard();
+			quizPosition++;		
+		}
 	}
-);	
+	
+	function rotateDartboard(){
+		$("#display-progress").css("transform","scale(1.9) rotate("+(quizPosition*30)+"deg)");
+		$("#dartboard-label").text(questions[quizPosition].title);	
+	}
+	
+	function resetQuizStatus(){
+		$("fieldset button").removeClass("active");
+		$("button.next").addClass("inactive");		
+	}
 
+ });
 });
-//chart displays reliably in lower left
-//chart rotates when you click next
-//data moves when you click next (no animation, rails model for quiz questions tbd)
-//questions stored as json probably
+//text label should show up over circle
 //chart lights up with appropriate light up
+//fix up the positioning, especially for the next button
