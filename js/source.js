aff85fc0230391d7053f23831af1aa279ddde5db
Nicole Thayer <nicole@carbonfive.com>
2017-02-20 17:33:01 -0800

    Results reliably display in bottom left

--- js/source.js
+++ js/source.js
@@ -1,6 +1,9 @@
 $( document ).ready(function() {								
 
 $("button.next").addClass("inactive");
+var buttonRating = 0;
+
+console.log($("div.response").width());
 
 $("fieldset button").click(
 	function(){
@@ -8,8 +11,15 @@ $("fieldset button").click(
 		$(this).prevAll().addClass("active");
 		$(this).nextAll().removeClass("active");
 		$("button.next").removeClass("inactive");
+		$("#display-progress").addClass("click");
+		buttonRating = $(this).text();
+		console.log($("#display-progress").children("g"));
 	}
 );	
 
-
 });
+//chart displays reliably in lower left
+//chart rotates when you click next
+//data moves when you click next (no animation, rails model for quiz questions tbd)
+//questions stored as json probably
+//chart lights up with appropriate light up
